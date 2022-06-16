f29d299fcddd4ce38dc36567d85976105c441148
pandu-supriyono <mail@supriyono.nl>
2022-06-16 07:04:08 +0200

    add top-level await for app start

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
 import { config } from '../config'
 import { createApp } from './app'
 
-const app = createApp()
+(async () => {
+  const app = createApp()
 
-app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`)
-})
+  app.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}`)
+  })
+})()
